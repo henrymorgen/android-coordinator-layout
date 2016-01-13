3b042a81cfc36406536234cfdcaf41de09ed1613
henrymorgen <525010411@qq.com>
2016-01-13 18:57:47 +0800

    修改代码

--- app/src/main/java/com/example/liuwangshu/mooncoordinatorlayout/CollapsingToolbarActivity.java
+++ app/src/main/java/com/example/liuwangshu/mooncoordinatorlayout/CollapsingToolbarActivity.java
@@ -1,5 +1,4 @@
 package com.example.liuwangshu.mooncoordinatorlayout;
-
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,13 +6,6 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-
-import java.util.List;
-
 public class CollapsingToolbarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +13,12 @@ public class CollapsingToolbarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         CollapsingToolbarLayout collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        collapsingToolbar.setTitle("我的哆啦");
+        collapsingToolbar.setTitle("哆啦A梦");
         RecyclerView mRecyclerView= (RecyclerView) findViewById(R.id.recyclerView);
         mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL));
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.setAdapter(new RecyclerViewAdapter(CollapsingToolbarActivity.this));
     }
-
 }
--- app/src/main/java/com/example/liuwangshu/mooncoordinatorlayout/CoordinatorLayoutActivity.java
+++ app/src/main/java/com/example/liuwangshu/mooncoordinatorlayout/CoordinatorLayoutActivity.java
@@ -1,33 +1,32 @@
 package com.example.liuwangshu.mooncoordinatorlayout;
-
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class CoordinatorLayoutActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     private TabLayout mTabLayout;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_tab_layout);
-            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
-            final ActionBar ab = getSupportActionBar();
-            ab.setDisplayHomeAsUpEnabled(true);
-            mViewPager = (ViewPager) findViewById(R.id.viewpager);
-            initViewPager();
-            }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_layout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        initViewPager();
+    }
+
     private void initViewPager() {
 
         mTabLayout = (TabLayout) findViewById(R.id.tabs);
@@ -45,11 +44,11 @@ public class CoordinatorLayoutActivity extends AppCompatActivity {
         titles.add("动漫");
         titles.add("搞笑");
         titles.add("精选");
-        for(int i=0;i<titles.size();i++){
+        for (int i = 0; i < titles.size(); i++) {
             mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(i)));
         }
         List<Fragment> fragments = new ArrayList<>();
-        for(int i=0;i<titles.size();i++){
+        for (int i = 0; i < titles.size(); i++) {
             fragments.add(new ListFragment());
         }
         FragmentAdapter mFragmentAdapteradapter =
@@ -62,10 +61,11 @@ public class CoordinatorLayoutActivity extends AppCompatActivity {
         mTabLayout.setTabsFromPagerAdapter(mFragmentAdapteradapter);
 
     }
+
     public void checkin(View view) {
         Snackbar.make(view, "点击成功", Snackbar.LENGTH_SHORT).show();
     }
 
-        }
+}
 
 
